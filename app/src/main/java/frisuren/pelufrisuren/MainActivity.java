ee52f957aeabf095c41635c57a0bbc6905e5fe9f
QuerolJuanManuel <juanmanuelqueroldolz@gmail.com>
2019-01-26 20:34:33 +0100

    Eliminado layout resgistro y añadido actyvity registro y funcion vincular al activity

--- app/src/main/java/frisuren/pelufrisuren/MainActivity.java
+++ app/src/main/java/frisuren/pelufrisuren/MainActivity.java
@@ -2,6 +2,8 @@ package frisuren.pelufrisuren;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registrarse(View view){
+        Intent i=new Intent(this, registro.class);
+        startActivity(i);
     }
 }
